2e166392acc38d6571312d1a6437ee8dfa24a522
Soumyajit Das <soumyajit.das@go-jek.com>
2021-02-27 14:22:35 +0530

    Basic customview CustomImageView creation

---
+++ app/src/main/java/com/example/customviewtutorial/customview/CustomImageView.kt
@@ -0,0 +1,15 @@
+package com.example.customviewtutorial.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.customviewtutorial.R
+
+class CustomImageView(
+    context: Context, attrs: AttributeSet?
+) : ConstraintLayout(context, attrs) {
+
+    init {
+        inflate(context, R.layout.layout_custom_imageview, this)
+    }
+}
